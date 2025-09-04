e25c1e2303bc72ff8a43137d92d1e05cac543300
Chandi977 <pappumahato000@gmail.com>
2025-09-04 21:32:56 +0530

    .

--- src/components/Login.js
+++ src/components/Login.js
@@ -34,7 +34,7 @@ const Login = ({ setAuth }) => {
       email: email.value.trim(),
       username: " ",
       password: password.value.trim(),
-    };
+      };
 
     try {
       // Use client() directly
